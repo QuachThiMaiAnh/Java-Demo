dab7edf47ffe0b615feb28a12550c6da2c8d69ba
QuachThiMaiAnh <quachthimaianh2002@gmail.com>
2024-03-16 08:35:45 +0700

    update lab1

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -1,41 +1,51 @@
 package Hello;
 
+import java.util.Scanner;
+
 public class Hello {
 	public static void main(String[] args) {
-		int a = 6;
-		int b = 9;
-		int c = a + b;
-		System.out.println("Sum: " + c);
-//		Kết thúc câu lệnh là dấu ;
-		int test = 20 + 9 * 3 - 10 / 2;
-//		Kết quả thì sd với dấu cộng
-		System.out.println("Kết quả là: " + test);
-//		In ra màn hình console
-//		Không xuống dòng
-		System.out.print("abc");
-//		Có xuống dòng
-		System.out.println("123");
-		System.out.println("1234");
-// 	In ra kết quả có định dạng: print format
-		System.out.printf("có %d người đăng ký ", 40000);
-		System.out.println();
-		System.out.printf("có %.3f người đăng ký", 40.123456);
-		System.out.println();
-		System.out.printf("có %sngười đăng ký", "Mai Anh ");
-		System.out.println();
-		System.out.println("Bài tập VD: ");
-		String name = "Mai Anh";
-		int age = 22;
-		System.out.println("-------------In ra kết quả có xuống dòng-------------");
-		System.out.println("Tên: " + name);
-		System.out.println("Tuổi: " + age);
-		System.out.println("-------------In ra kết quả mà không xuống dòng-------------");
-		System.out.print("Tên: " + name);
-		System.out.print("Tuổi: " + age);
-		System.out.println();
-		System.out.println("-------------In ra kết quả có định dạng format-------------");
-		System.out.printf("Tên là %s ", name);
-		System.out.println();
-		System.out.printf("Tuổi bằng %d ", age);
+//		int a = 6;
+//		int b = 9;
+//		int c = a + b;
+//		System.out.println("Sum: " + c);
+////		Kết thúc câu lệnh là dấu ;
+//		int test = 20 + 9 * 3 - 10 / 2;
+////		Kết quả thì sd với dấu cộng
+//		System.out.println("Kết quả là: " + test);
+////		In ra màn hình console
+////		Không xuống dòng
+//		System.out.print("abc");
+////		Có xuống dòng
+//		System.out.println("123");
+//		System.out.println("1234");
+//// 	In ra kết quả có định dạng: print format
+//		System.out.printf("có %d người đăng ký ", 40000);
+//		System.out.println();
+//		System.out.printf("có %.3f người đăng ký", 40.123456);
+//		System.out.println();
+//		System.out.printf("có %s người đăng ký", "Mai Anh ");
+//		System.out.println();
+//		System.out.println("Bài tập VD: ");
+//		String name = "Mai Anh";
+//		int age = 22;
+//		System.out.println("-------------In ra kết quả có xuống dòng-------------");
+//		System.out.println("Tên: " + name);
+//		System.out.println("Tuổi: " + age);
+//		System.out.println("-------------In ra kết quả mà không xuống dòng-------------");
+//		System.out.print("Tên: " + name + "\nTuổi: " + age);
+//		System.out.println();
+//		System.out.println("-------------In ra kết quả có định dạng format-------------");
+//		System.out.printf("Tên là %s ", name);
+//		System.out.println();
+//		System.out.printf("Tuổi bằng %d ", age);
+//		java.util.Scanner là công cụ được java hỗ trợ sẵn, dùng để nhập dữ liệu từ bàn phím
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhập vào số a=  ");
+		int a = scanner.nextInt();
+		System.out.println("Nhập vào số b=  ");
+		int b = scanner.nextInt();
+		System.out.println("Số lớn hơn là :" + Math.pow(a, b));
+		scanner.close();
+
 	}
 }
---
+++ src/Hello/Hello1.java
@@ -0,0 +1,18 @@
+package Hello;
+
+import java.util.Scanner;
+
+public class Hello1 {
+	public static void main(String[] args) {
+//	Thư viện cho phép nhận một gì đó từ bàn phím
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhập tên sinh viên: ");
+		String name = scanner.nextLine();
+		System.out.println("Nhập điểm trung bình: ");
+		double score = scanner.nextDouble();
+		System.out.println(name + " có điểm trung bình =  " + score);
+		System.out.println("Tên: " + name);
+//		Rint làm tròn lên
+		System.out.println("Điểm = " + Math.rint(score));
+	}
+}
